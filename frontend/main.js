9932ea139161eb0af36b7d02d39d90d81def31ef
Gagan2699 <gagandeep262699@gmail.com>
2021-06-24 23:32:06 -0400

    Create main.js

---
+++ frontend/main.js
@@ -0,0 +1,12 @@
+const functionApi=''
+
+const getVisitCount =() => {
+    let count =30;
+    fetch(functionApi).then(response=>{
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function Api.");
+        count = response.count;
+        document.getElementById("counter").innerText=count;
+    })
+}
